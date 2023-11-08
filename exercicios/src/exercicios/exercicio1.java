90a2e2ed0368cd5a18be369977c6f37b68a709d4
Renan Nunes <rafaelnc22@gmail.com>
2023-11-08 15:46:22 -0300

    Exercicios Operacoes

---
+++ exercicios/src/exercicios/exercicio1.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float salario,abono,novoSalario;
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite seu salário: ");
+		salario = leia.nextFloat();
+		
+		System.out.println("Digite o abono: ");
+		abono = leia.nextFloat();
+		
+		novoSalario = salario + abono;
+		System.out.printf("\nNovo Salário do colaborador: %.2f", novoSalario);
+		
+       
+       
+	}
+
+}
